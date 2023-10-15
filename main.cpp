42a28e66508438118e580eb7781cf57090c51ca5
Anurag Ray Chowdhury <110551628+anuragraychowdhury@users.noreply.github.com>
2023-10-15 17:15:01 -0500

    initial commit of c++ hello

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include <iostream>
+
+int main(int argc, char* argv[]) //argc = argument count, argv = argument value; array of strings with length arg c
+{
+    std::cout << "hello";
+}
